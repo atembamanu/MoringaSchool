0f66c0862e5234da08a0b23e19ad9009bd8626fa
atembamanu <atembaemm07@gmail.com>
2019-08-07 08:24:18 +0300

    Initial commit

---
+++ myfeeling.bash
@@ -0,0 +1,14 @@
+let sorry = gloomy;
+sorry = sorry + 2499synonyms;
+
+
+let upbeat = happy;
+upbeat = upbeat + 366antonyms;
+
+if(!sorry){
+	alert("Today I'm felling " +upbeat+ "cos you made it to day Three! of Coding." );
+	alert("Happy Coding");
+
+}else{
+	alert("I'm so "+sorry+ " If you find this confusing, but its just a mood logic which doesnt print anything");
+}
